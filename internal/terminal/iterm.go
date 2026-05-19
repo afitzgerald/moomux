4b6889a611944f1b24910c963ddbb7065e89e0dd
“Erick <erickgoncalves5@hotmail.com>
2026-05-19 18:38:31 -0300

    feat: add iTerm2 backend to terminal package

---
+++ internal/terminal/iterm.go
@@ -0,0 +1,86 @@
+package terminal
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+type scriptRunner interface {
+	Run(script string) (string, error)
+}
+
+type execScriptRunner struct{}
+
+func (execScriptRunner) Run(script string) (string, error) {
+	out, err := exec.Command("osascript", "-e", script).CombinedOutput()
+	return string(out), err
+}
+
+type itermClient struct {
+	runner scriptRunner
+}
+
+func newITermClient() *itermClient {
+	return &itermClient{runner: execScriptRunner{}}
+}
+
+func (c *itermClient) OpenSession(tmuxSession, title string) error {
+	setName := ""
+	if title != "" {
+		setName = fmt.Sprintf("\n\t\t\tset name to \"%s\"", escapeAppleScript(title))
+	}
+	script := fmt.Sprintf(`
+tell application "iTerm2"
+	activate
+	if (count of windows) = 0 then
+		create window with default profile
+	end if
+	tell current window
+		create tab with default profile
+		tell current session of current tab%s
+			write text "tmux attach -t %s"
+		end tell
+	end tell
+end tell`, setName, tmuxSession)
+	_, err := c.runner.Run(script)
+	return err
+}
+
+func escapeAppleScript(s string) string {
+	out := make([]rune, 0, len(s))
+	for _, r := range s {
+		if r == '\\' || r == '"' {
+			out = append(out, '\\')
+		}
+		out = append(out, r)
+	}
+	return string(out)
+}
+
+// Stub implementations for other terminal types (to be implemented in later tasks)
+type windowOpener struct {
+	binary string
+	args   func(string) []string
+}
+
+func (w *windowOpener) OpenSession(tmuxSession, title string) error {
+	return nil
+}
+
+type fallbackOpener struct{}
+
+func (f *fallbackOpener) OpenSession(tmuxSession, title string) error {
+	return nil
+}
+
+func kittyArgs(session string) []string {
+	return nil
+}
+
+func weztermArgs(session string) []string {
+	return nil
+}
+
+func terminalAppArgs(session string) []string {
+	return nil
+}
